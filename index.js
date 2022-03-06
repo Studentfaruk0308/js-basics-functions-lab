e98e29948f2a2c36c2eed3a7fd7dbc6b22701d17
Studentfaruk0308 <studentfaruk0308@gmail.com>
2022-03-07 10:25:37 +1100

    js-basics-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+        return (Math.abs(someValue - 42));
+  }
+
+function distanceFromHqInFeet (someValue) {
+    return ((Math.abs(someValue - 42))*264);
+}
+
+function distanceTravelledInFeet (x, y) {
+    return ((Math.abs(x-y))*264);
+}
+
+function calculatesFarePrice(x, y){
+    
+    if (distanceTravelledInFeet(x,y) <= 400){
+        return 0;
+    }
+    else if(distanceFromHqInFeet(x,y) >400 && distanceTravelledInFeet(x,y) <=2000){
+        return ((distanceTravelledInFeet(x,y))-400) * 0.02;
+    } 
+    else if(distanceTravelledInFeet(x,y) >2000 && distanceTravelledInFeet(x,y) < 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
+
